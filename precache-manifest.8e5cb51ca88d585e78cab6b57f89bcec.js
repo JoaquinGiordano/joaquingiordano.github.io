2bbbf474a2cad60e520d44d08ff1177865761708
Joaquin Giordano <joaquin_giordano@outlook.com>
2020-05-03 01:16:10 -0300

    Updates

---
+++ precache-manifest.8e5cb51ca88d585e78cab6b57f89bcec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffafab8a0698479c2ce6f075dac0b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4048bff00c0c32c25a8",
+    "url": "/static/css/main.0b7fda9c.chunk.css"
+  },
+  {
+    "revision": "66fcc7518c48afab57e6",
+    "url": "/static/js/2.9fc1886b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.9fc1886b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4048bff00c0c32c25a8",
+    "url": "/static/js/main.24fa8d95.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
